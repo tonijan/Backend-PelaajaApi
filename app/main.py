743907c273c950eb6da1f1713030f98ba47541ab
Toni Jantunen <viinitin@gmail.com>
2023-04-28 18:17:28 +0300

    Able to receive requests from any origin

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from .routers import players, events
-
 from .database.database import engine
 from .database import models
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(players.router)
 app.include_router(events.router)
